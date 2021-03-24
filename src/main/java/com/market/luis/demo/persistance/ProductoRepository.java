2256fe074d1760e6fc4e688348cbab9c74c00a68
Luis Fraga <luis5944@gmail.com>
2021-03-24 10:56:11 +0100

    persistence

---
+++ src/main/java/com/market/luis/demo/persistance/ProductoRepository.java
@@ -0,0 +1,37 @@
+package com.market.luis.demo.persistance;
+
+import com.market.luis.demo.persistance.crud.ProductoCrudRepository;
+import com.market.luis.demo.persistance.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidadStock) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
---
+++ src/main/java/com/market/luis/demo/persistance/crud/ProductoCrudRepository.java
@@ -0,0 +1,14 @@
+package com.market.luis.demo.persistance.crud;
+
+import com.market.luis.demo.persistance.entity.Producto;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
